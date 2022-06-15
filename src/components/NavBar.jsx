5e3efdf21b94dc92e828ab71bd3ba56f4eed67a4
ubaldo-cervantes <uvaldocervante57@gmail.com>
2022-06-15 18:25:44 -0500

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='navbar bg-dark mb-5'>
+      <div className='container-fluid'>
+        <h3 className='text-light mx-auto'> Prediccion Meteorologica</h3>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
